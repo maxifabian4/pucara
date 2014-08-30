5819c52f6d91a2720a1ec1111a1450381110f94f
maxifabian4 <maxifabian4@2fd35d72-0aa2-9f4c-6789-423f39d6c511>
2014-08-30 16:18:26 +0000

    Initial project has been created.

---
+++ src/main/java/com/pucara/app/App.java
@@ -0,0 +1,13 @@
+package com.pucara.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
