a23d4863f480be703905d4004550f4e45c59c004
maxifabian4 <maxifabian4@2fd35d72-0aa2-9f4c-6789-423f39d6c511>
2014-09-04 22:45:13 +0000

    Properties management added.

--- src/test/java/com/pucara/mysql/MysqlConnectionTest.java
+++ src/test/java/com/pucara/mysql/MysqlConnectionTest.java
@@ -11,24 +11,35 @@ import org.slf4j.LoggerFactory;
 
 import junit.framework.TestCase;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Unit test for simple App.
  */
 public class MysqlConnectionTest extends TestCase {
-	private static final Logger LOGGER = LoggerFactory.getLogger(MysqlConnectionTest.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(MysqlConnectionTest.class);
 
-	public void testConnection() {
-		String dbUrl = "jdbc:mysql://192.168.1.69:3306/pucaratest";
-		String dbClass = "com.mysql.jdbc.Driver";
-		// String query =
-		// "select distinct(table_name) from INFORMATION_SCHEMA.TABLES";
-		String query = "select description from product";
-		String username = "root";
-		String password = "pucara";
+	public void testConnection() throws IOException {
+		Properties prop = new Properties();
+		InputStream input = new FileInputStream(
+				"src/main/resources/properties/db.properties");
+		// Load a properties file.
+		prop.load(input);
+
+		String dbUrl = prop.getProperty("db.url");
+		String dbClass = prop.getProperty("db.class");
+		String query = prop.getProperty("db.query");
+		String username = prop.getProperty("db.username");
+		String password = prop.getProperty("db.password");
 
 		try {
 			Class.forName(dbClass);
-			Connection connection = DriverManager.getConnection(dbUrl, username, password);
+			Connection connection = DriverManager.getConnection(dbUrl,
+					username, password);
 			Statement statement = connection.createStatement();
 			ResultSet resultSet = statement.executeQuery(query);
 
